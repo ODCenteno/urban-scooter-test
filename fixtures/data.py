5c16ffafabfb79efb2daa3221a4f67bea937f242
= <pixie_cemodan@hotmail.com>
2023-10-25 23:20:00 -0700

    First commit & set architecture

---
+++ fixtures/data.py
@@ -0,0 +1,18 @@
+VALID_DATA = {
+    "user_details": {
+        "name": "Daniel",
+        "last_name": "Montalvo",
+        "address": "Camelias 123",
+        "phone": "+ 1 123 234 345"
+    }
+}
+
+INVALID_DATA = {
+    "empty_str": "",
+    "user_details": {
+        "name": "Daniel",
+        "last_name": "Montalvo",
+        "address": "Camelias 123",
+        "phone": "+ 1 123 234 345"
+    }
+}
